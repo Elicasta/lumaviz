eb93a64eb8ec28af07f8266f0e170e461e206f11
Elicasta <130819591+Elicasta@users.noreply.github.com>
2026-09-23 14:11:54 -0400

    Tree-shake Babylon material imports

--- src/viz/materials.ts
+++ src/viz/materials.ts
@@ -1,4 +1,6 @@
-import { Color3, PBRMaterial, Scene } from "@babylonjs/core";
+import { Color3 } from "@babylonjs/core/Maths/math.color";
+import { PBRMaterial } from "@babylonjs/core/Materials/PBR/pbrMaterial";
+import type { Scene } from "@babylonjs/core/scene";
 import type { MaterialPreset } from "./types";
 
 export interface SceneMaterials {
